d9323093a9d39fb3e9eefef0142c9c7ffbecef7d
Sebastian009w <ia3266595@gmail.com>
2023-03-10 22:00:36 -0500

    se definen endspoint y los controllers

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,8 +1,16 @@
 import { Router } from "express";
+import { singUp, singIn, profile } from "../controllers/auth.controller";
 
 /**
  * @module router
  */
 const router: Router = Router();
+
+/**
+ * @description auth
+ */
+router.post("/singin", singIn);
+router.post("/singup", singUp);
+router.get("/profile", profile);
 
 export { router };
