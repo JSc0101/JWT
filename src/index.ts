8073c54574b70cf81e01dd7ec6a71dd5f45f5c72
Sebastian009w <ia3266595@gmail.com>
2023-03-10 21:44:39 -0500

    coneccion a monogoDb exitosa

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import app from "./app";
+import { conection } from "./database";
 
 /**
  * @name main
  */
-(() => {
+(async () => {
   app.listen(app.get("port"));
+  await conection();
   console.log("runtime");
 })();
